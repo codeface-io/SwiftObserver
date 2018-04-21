c204158139e1041bb9189f0236f3c892361efa2d
Sebastian <contact@flowtoolz.com>
2018-04-22 00:36:36 +0200

    add cocoapods example project

---
+++ Example Projects/Cocoapods/iOS/SwiftObserverExampleCocoapodsIOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftObserverExampleCocoapodsIOS
+//
+//  Created by Sebastian ******** on 22.04.18.
+//  Copyright © 2018 Flowtoolz. All rights reserved.
+//
+
+import UIKit
+import SwiftObserver
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
