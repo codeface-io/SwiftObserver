2ac8eadbb025b029680c9c9e9411b53e1df0915e
Sebastian <contact@flowtoolz.com>
2018-05-05 19:32:53 +0200

    make Unwrap internal. update type dependency diagram.

--- Code/Unwrap.swift
+++ Code/Unwrap.swift
@@ -1,21 +1,21 @@
 public extension ObservableProtocol
 {
-    public func unwrap<Unwrapped>(_ defaultUpdate: Unwrapped) -> Unwrap<Self, Unwrapped>
+    public func unwrap<Unwrapped>(_ defaultUpdate: Unwrapped) -> Mapping<Self, Unwrapped>
+        where Self.UpdateType == Optional<Unwrapped>
     {
-        return Unwrap(observable: self,
-                                defaultUpdate: defaultUpdate)
+        return Unwrap(observable: self, defaultUpdate: defaultUpdate)
     }
 }
 
-public class Unwrap<SourceObservable: ObservableProtocol, Unwrapped>: Mapping<SourceObservable, Unwrapped> where SourceObservable.UpdateType == Optional<Unwrapped>
+class Unwrap<SourceObservable: ObservableProtocol, Unwrapped>: Mapping<SourceObservable, Unwrapped> where SourceObservable.UpdateType == Optional<Unwrapped>
 {
     init(observable: SourceObservable, defaultUpdate: Unwrapped)
     {
         super.init(observable: observable) { $0 ?? defaultUpdate }
     }
     
-    public override func add(_ observer: AnyObject,
-                             _ receive: @escaping UpdateReceiver)
+    override func add(_ observer: AnyObject,
+                      _ receive: @escaping UpdateReceiver)
     {
         observable?.add(observer)
         {
