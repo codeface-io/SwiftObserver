8c2edb4fc6f1637b6764ae1a3dc3c76deedf9ac1
Sebastian <contact@flowtoolz.com>
2018-05-10 23:34:00 +0200

    proof concept: make UI element observable without requiring it to derive from any class

--- Tests/SwiftObserverTests.swift
+++ Tests/SwiftObserverTests.swift
@@ -2,6 +2,29 @@ import XCTest
 import SwiftObserver
 import UIKit
 
+extension UIButton: Observable
+{
+    public var latestUpdate: UIControlEvents? { return nil }
+    
+    public func add(_ observer: AnyObject,
+                    _ receive: @escaping (UpdateType) -> Void)
+    {
+        ObservationService.add(observer, of: self, receive)
+        
+        guard !allTargets.contains(self) else { return }
+        
+        addTarget(self, action: #selector(sendTouchDown), for: .touchDown)
+        addTarget(self, action: #selector(sendTouchUpInside), for: .touchUpInside)
+        addTarget(self, action: #selector(sendTouchUpOutside), for: .touchUpOutside)
+        addTarget(self, action: #selector(sendTouchCancel), for: .touchCancel)
+    }
+    
+    @objc private func sendTouchDown() { send(.touchDown) }
+    @objc private func sendTouchUpInside() { send(.touchUpInside) }
+    @objc private func sendTouchUpOutside() { send(.touchUpOutside) }
+    @objc private func sendTouchCancel() { send(.touchCancel) }
+}
+
 extension UILabel: TextPresenter
 {
     var presentedText: String?
