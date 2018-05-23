3a567ad783fb4ad463a9d877953f7c87291a65e6
Sebastian <contact@flowtoolz.com>
2018-05-23 10:50:18 +0200

    remove message selection

--- Code/Messenger.swift
+++ Code/Messenger.swift
@@ -1,27 +1,10 @@
 public let textMessenger = Messenger("")
 
-public extension Observer
-{
-    func observe<Message>(_ message: Message,
-                          from messenger: Messenger<Message>,
-                          receive: @escaping () -> Void)
-    {
-        messenger.add(self, for: message, receive: receive)
-    }
-}
-
 public class Messenger<Message: Equatable>: Observable
 {
     public init(_ latest: Message)
     {
         storedLatestMessage = latest
-    }
-    
-    public func add(_ observer: AnyObject,
-                    for message: Message,
-                    receive: @escaping () -> Void)
-    {
-        add(observer, filter: { $0 == message }) { _ in receive() }
     }
     
     public func send(_ update: Message)
