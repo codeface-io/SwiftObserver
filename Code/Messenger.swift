799ed86620b5a7e40466946d9cd545ce98714b15
Sebastian <contact@flowtoolz.com>
2018-12-13 17:20:10 +0100

    Adjust Messenger

--- Code/Messenger.swift
+++ Code/Messenger.swift
@@ -1,23 +1,16 @@
-public class Messenger<Message>: ObservableObject<Message>
+public class Messenger<Message>: ObservableObject<Message?>
 {
-    init(initialMessage: Message)
-    {
-        message = initialMessage
-        
-        super.init()
-    }
-    
-    public override func send(_ update: Message)
+    public override func send(_ update: Message?)
     {
         message = update
         
         super.send(update)
     }
     
-    public override var latestUpdate: Message
+    public override var latestUpdate: Message?
     {
         return message
     }
     
-    private var message: Message
+    private var message: Message?
 }
