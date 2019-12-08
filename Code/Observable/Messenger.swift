f0403ef604b34901ed3f95e6b384348844778827
flowtoolz <contact@flowtoolz.com>
2019-12-08 13:18:03 +0100

    Extract class ReceiverPool from Messenger

--- Code/Observable/Messenger.swift
+++ Code/Observable/Messenger.swift
@@ -8,19 +8,13 @@ public final class Messenger<Message>
     
     public init() {}
     
-    deinit
-    {
-        registrations.values.forEach { $0.connection?.releaseFromReceiver() }
-    }
-    
-    // MARK: - Send Messages to Connected Receivers
+    // MARK: - Send Messages to Receivers
     
     internal func send(_ message: Message, author: AnyAuthor)
     {
         guard maintainsMessageOrder else
         {
-            sendDequeued(message, author: author)
-            return
+            return receivers.receive(message, from: author)
         }
         
         messagesFromAuthors.append((message, author))
@@ -29,41 +23,19 @@ public final class Messenger<Message>
         
         while let (message, author) = messagesFromAuthors.first
         {
-            sendDequeued(message, author: author)
+            receivers.receive(message, from: author)
             messagesFromAuthors.removeFirst()
-        }
-    }
-    
-    private func sendDequeued(_ message: Message, author: AnyAuthor)
-    {
-        registrations.forEach
-        {
-            (receiverKey, registration) in
-            
-            guard let connection = registration.connection else
-            {
-                registrations[receiverKey] = nil
-                return log(error: "Tried to send message via dead connection.")
-            }
-            
-            guard connection.receiver != nil else
-            {
-                registrations[receiverKey] = nil
-                return log(error: "Tried to send message to dead receiver.")
-            }
-            
-            registration.receive(message, author)
         }
     }
     
     private var messagesFromAuthors = [(Message, AnyAuthor)]()
     public var maintainsMessageOrder = true
     
-    // MARK: - Connect Receivers
+    // MARK: - Manage Receivers
     
     internal func isConnected(to receiver: ReceiverInterface) -> Bool
     {
-        registrations[receiver.key]?.connection?.receiver === receiver
+        receivers.contains(receiver)
     }
     
     internal func register(_ connection: Connection,
@@ -80,42 +52,13 @@ public final class Messenger<Message>
             log(error: "\(Self.self) will register a connection that points to a different \(Self.self).")
         }
         
-        let registration = ConnectionRegistration(connection: connection, receive: receive)
-        registrations[connection.receiverKey] = registration
+        receivers.add(connection, receive: receive)
     }
-
-    // MARK: - Connections
     
     internal func unregister(_ connection: ConnectionInterface)
     {
-        let receiverKey = connection.receiverKey
-        
-        guard let registeredConnection = registrations[receiverKey]?.connection else
-        {
-            return
-        }
-        
-        guard registeredConnection === connection else
-        {
-            return log(error: "Tried to unregister a connection with an outdated reused receiver key. This can only happen if \(Receiver.self) is retained outside its owning Observer after the Observer has died. You're not supposed to do anything with the \(Receiver.self) object, let alone retain it.")
-        }
-        
-        registrations[receiverKey] = nil
+        receivers.remove(connection)
     }
     
-    private var registrations = [ReceiverKey : ConnectionRegistration]()
-    
-    private class ConnectionRegistration
-    {
-        init(connection: Connection, receive: @escaping (Message, AnyAuthor) -> Void)
-        {
-            self.connection = connection
-            self.receive = receive
-        }
-        
-        weak var connection: Connection?
-        let receive: (Message, _ from: AnyAuthor) -> Void
-    }
+    private let receivers = ReceiverPool<Message>()
 }
-
-public typealias AnyAuthor = AnyObject
