960d5eaab958fff8e6a130a091dda190e0de7cc6
flowtoolz <contact@flowtoolz.com>
2019-12-10 00:32:30 +0100

    Roll back conditional conformance of Filter to BufferedObservable
    ... this would possibly change the semantics of the filter (too much)

--- Code/Observable/Transforms/Filter+BufferedObservable.swift
+++
@@ -1,9 +0,0 @@
-extension Filter: BufferedObservable
-    where O: BufferedObservable, O.Message == Optional<Any>
-{
-    public var latestMessage: O.Message
-    {
-        let latestSourceMessage = observable.latestMessage
-        return keep(latestSourceMessage) ? latestSourceMessage : nil
-    }
-}
