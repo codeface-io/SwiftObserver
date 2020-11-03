86d7b0b0fad0ff60c9e3ad18871c8880fe9a59b0
flowtoolz <contact@flowtoolz.com>
2020-11-03 23:11:04 +0100

    Add variant of whenFulfilled that provides the author of the fulfilling message

--- Code/Observable/BufferedObservable+GetUnwrappedMessage.swift
+++ Code/Observable/BufferedObservable+GetUnwrappedMessage.swift
@@ -21,4 +21,26 @@ public extension BufferedObservable
             }
         }
     }
+    
+    func whenFulfilled<Unwrapped>(_ receive: @escaping (Unwrapped, AnyAuthor) -> Void)
+        where Message == Unwrapped?
+    {
+        if let message = latestMessage
+        {
+            receive(message, latestAuthor)
+        }
+        else
+        {
+            let fulfillmentObserver = AdhocObserver()
+            
+            fulfillmentObserver.observe(self).unwrap
+            {
+                unwrapped, author in
+                
+                fulfillmentObserver.stopObserving()
+                
+                receive(unwrapped, author)
+            }
+        }
+    }
 }
