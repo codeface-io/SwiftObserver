f2279a1695260da21d0cdca19ad272e408676692
flowtoolz <contact@flowtoolz.com>
2020-11-03 23:07:24 +0100

    Add FIXME

--- Code/Observable/Transforms/Mapper+BufferedObservable.swift
+++ Code/Observable/Transforms/Mapper+BufferedObservable.swift
@@ -1,5 +1,6 @@
 extension Mapper: BufferedObservable where O: BufferedObservable
 {
+    // FIXME: this doesn't really do it in case someone calls send on the mapper directly. the mapper would need to be a Messenger and track all the sent messages and authors like Buffer does. but then all mappers would be buffered and have that redundant message store .... so better document this possibly unexpected behaviour of the "buffered" Mapper.
     public var latestMessage: Mapped
     {
         map(observable.latestMessage)
