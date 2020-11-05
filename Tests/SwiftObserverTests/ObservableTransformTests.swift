410bf7addb4cd1cb02d4c3669745d9c84037961a
flowtoolz <contact@flowtoolz.com>
2020-11-05 12:29:13 +0100

    Log warning when buffering a buffer, and unit-test that

--- Tests/SwiftObserverTests/ObservableTransformTests.swift
+++ Tests/SwiftObserverTests/ObservableTransformTests.swift
@@ -1,8 +1,15 @@
 import XCTest
 @testable import SwiftObserver
+import SwiftyToolz
 
-class ObservableTransformTests: XCTestCase
+class ObservableTransformTests: XCTestCase, LogObserver
 {
+    static override func setUp()
+    {
+        super.setUp()
+        Log.shared.minimumLevel = .error
+    }
+    
     func testBufferLatestMessageIsOptionalOnObservableWithOptionalMessage()
     {
         let buffer = Messenger<Int?>().buffer()
@@ -14,4 +21,29 @@ class ObservableTransformTests: XCTestCase
         let buffer = Messenger<Int>().buffer()
         XCTAssert(type(of: buffer.latestMessage) == Int?.self)
     }
+    
+    func testLogWarningWhenApplyingBufferToBufferWithNonOptionalMessage()
+    {
+        Log.shared.add(observer: self)
+        let alreadyBuffered = Var<Int?>()
+        let expectedWarning = alreadyBuffered.warningWhenApplyingBuffer(messageIsOptional: false)
+        _ = alreadyBuffered.buffer()
+        XCTAssert(latestLogEntry?.message.contains(expectedWarning) ?? false)
+        Log.shared.remove(observer: self)
+    }
+    
+    func testLogWarningWhenApplyingBufferToBufferWithOptionalMessage()
+    {
+        Log.shared.add(observer: self)
+        latestLogEntry = nil
+        let alreadyBuffered = Messenger<Int>().buffer()
+        XCTAssertNil(latestLogEntry)
+        let expectedWarning = alreadyBuffered.warningWhenApplyingBuffer(messageIsOptional: true)
+        _ = alreadyBuffered.buffer()
+        XCTAssert(latestLogEntry?.message.contains(expectedWarning) ?? false)
+        Log.shared.remove(observer: self)
+    }
+    
+    func receive(_ entry: Log.Entry) { latestLogEntry = entry }
+    private var latestLogEntry: Log.Entry?
 }
